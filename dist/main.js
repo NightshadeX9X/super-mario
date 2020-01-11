5bbf3b4a126f1acbbf57bbbb16fc569df0ae4453
NightshadeX9X <solgaliumz@gmail.com>
2020-01-11 20:15:27 +0100

    Added Loading... Message and canvas

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,5 @@
 "use strict";
+let cnv = document.createElement('canvas');
+let ctx = cnv.getContext('2d');
+document.body.innerHTML = "";
+document.body.appendChild(cnv);
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+let cnv = document.createElement('canvas');
+let ctx = <CanvasRenderingContext2D>cnv.getContext('2d');
+document.body.innerHTML = "";
+document.body.appendChild(cnv);
