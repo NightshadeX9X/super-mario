a186cefea385263fa8ab5186c9e21475563641f2
NightshadeX9X <solgaliumz@gmail.com>
2020-01-12 19:12:22 +0100

    created loadBackgroundSprites func

--- dist/main.js
+++ dist/main.js
@@ -7,20 +7,22 @@ import Spritesheet from './Spritesheet.js';
 let loader = new Loader();
 loader.paths.add('image', 'images/');
 let progressBar = document.getElementById('loadingBar');
-(async function () {
-    let promises = [
-        handleImport('./levels/1-1.js'),
-        loader.image('tileset.png'),
-    ];
-    progressBar.setAttribute('max', `${promises.length}`);
-    let [level_import, image] = await promiseEvery(promises, loaded => { progressBar.value++; console.log("Loaded:", loaded); });
-    let level = level_import.default;
-    console.log("Here", level, level_import, image);
-    console.log("All necessary assets loaded.");
-    let { cnv, ctx } = createCanvas(new Vector(480, 240), true, true);
+async function loadBackgroundSprites() {
+    let image = await loader.image('tileset.png');
     let backgroundSprites = new Spritesheet(image, new Vector(16));
     backgroundSprites.registerSprite('ground', new Vector());
     backgroundSprites.registerSprite('sky', new Vector(3, 23));
+}
+(async function () {
+    let promises = [
+        handleImport('./levels/1-1.js'),
+        loadBackgroundSprites()
+    ];
+    progressBar.setAttribute('max', `${promises.length}`);
+    let [level_import, backgroundSprites] = await promiseEvery(promises, loaded => { progressBar.value++; console.log("Loaded:", loaded); });
+    let level = level_import.default;
+    console.log("All necessary assets loaded.");
+    let { cnv, ctx } = createCanvas(new Vector(480, 240), true, true);
     level.backgrounds.forEach(bg => {
         for (let y = bg.ranges[0]; y < bg.ranges[1]; y++) {
             for (let x = bg.ranges[2]; x < bg.ranges[3]; x++) {
--- src/main.ts
+++ src/main.ts
@@ -10,22 +10,27 @@ let loader = new Loader();
 loader.paths.add('image', 'images/')
 let progressBar = <HTMLProgressElement>document.getElementById('loadingBar');
 
+async function loadBackgroundSprites() {
+    let image = await loader.image('tileset.png')
+
+    let backgroundSprites = new Spritesheet(image, new Vector(16));
+    backgroundSprites.registerSprite('ground', new Vector());
+    backgroundSprites.registerSprite('sky', new Vector(3, 23));
+}
+
 (async function () {
     let promises: any =
         [
             handleImport('./levels/1-1.js'),
-            loader.image('tileset.png'),
+            loadBackgroundSprites()
         ];
     progressBar.setAttribute('max', `${promises.length}`);
-    let [level_import, image] =
-        <[any, HTMLImageElement]>await promiseEvery(promises, loaded => { progressBar.value++; console.log("Loaded:", loaded) });
+    let [level_import, backgroundSprites] =
+        <[any, Spritesheet]>await promiseEvery(promises, loaded => { progressBar.value++; console.log("Loaded:", loaded) });
     let level: Level = level_import.default;
-    console.log("Here", level, level_import, image)
     console.log("All necessary assets loaded.")
     let { cnv, ctx } = createCanvas(new Vector(480, 240), true, true);
-    let backgroundSprites = new Spritesheet(image, new Vector(16));
-    backgroundSprites.registerSprite('ground', new Vector());
-    backgroundSprites.registerSprite('sky', new Vector(3, 23));
+
     level.backgrounds.forEach(bg => {
         for (let y = bg.ranges[0]; y < bg.ranges[1]; y++) {
             for (let x = bg.ranges[2]; x < bg.ranges[3]; x++) {
